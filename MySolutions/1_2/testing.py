53aee5cd5125eb1573037c7acbf97936b3569d40
AlexK <alex.keller2001@gmail.com>
2025-12-13 15:05:10 +0100

    exercieses 1.2 and 1.3

---
+++ MySolutions/1_2/testing.py
@@ -0,0 +1,3 @@
+# messed around a little with builtins.
+# more of a refresher than anything else so far.
+
---
+++ MySolutions/1_3/pcost.py
@@ -0,0 +1,13 @@
+total_cost = 0.0
+
+with open("../../Data/portfolio.dat") as f:
+    for line in f:
+        data = line.split(" ")
+        stock_name = data[0]
+        stock_count = float(data[1])
+        stock_price = float(data[2])
+        local_stock_total = stock_count * stock_price
+        print(f"{stock_name}: {local_stock_total:>10.2f}")
+        total_cost += local_stock_total
+
+print(f"Total stock cost {total_cost:>10.2f}")
